dbf9539f20b0021ff4f956f9548a89171074cbf4
JOHN CENA <patilneeraj2003@gmail.com>
2023-01-10 21:02:31 +0530

    project-setup

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <center>
+        Hello This is about page
+      </center>  
+    </div>
+  )
+}
+
+export default about
